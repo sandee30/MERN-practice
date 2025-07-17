b7d1c7652a78ae03a7e7bd36ec4a3f27c87dbee3
Sandeep.23 <sandeep.of.30@gmail.com>
2025-07-17 23:15:24 +0545

    Remove node_modules from tracking

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.set("view engine","ejs");
 
 app.get('/', (req, res)=> {
     res.render("index");
 })
 
-PORT = 3000;
+app.post('/get-form-data', (req, res)=>{
+    console.log(req.body);
+    res.send("data recieved.");
+})
+
+const PORT = 3000;
 
 app.listen(PORT, ()=>{
     console.log(`server running at port: ${PORT}`)
